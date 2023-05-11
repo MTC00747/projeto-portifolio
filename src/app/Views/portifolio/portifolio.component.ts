e0d0628a5e63a8ea5a5b69c23910e708dad2cfe5
MTC <matheustcavalcanti@hotmail.com>
2023-05-11 17:40:42 -0300

    Finalizando componente de portifólio

--- src/app/Views/portifolio/portifolio.component.ts
+++ src/app/Views/portifolio/portifolio.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./portifolio.component.css']
 })
 export class PortifolioComponent {
+//
+
+  projetos = [
+    {
+    Imagem : "./assets/casaDosLivros.png",
+    Titulo :"CasaDosLivros",
+    Descricao : "Precisa de um E-commerce de livros?",
+    Link : "https://github.com/MTC00747/CasaDosLivrosAngular"
+    }
+    ,{
+      Imagem : "./assets/casaDosLivros.png",
+    Titulo :"Blog-Angular",
+    Descricao : "Precisa de um Blog?",
+    Link : "https://github.com/MTC00747/CasaDosLivrosAngular"
+    }
+  ]
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './Views/menu-bar/menu-bar.component';
-import { HomeComponent } from './views/home/home.component';
-import { CardTituloComponent } from './views/card-titulo/card-titulo.component';
-import { SobreMimComponent } from './views/sobre-mim/sobre-mim.component';
-import { CardSkillsComponent } from './views/card-skills/card-skills.component';
+import { HomeComponent } from './Views/home/home.component';
+import { CardTituloComponent } from './Views/card-titulo/card-titulo.component';
+import { SobreMimComponent } from './Views/sobre-mim/sobre-mim.component';
+import { CardSkillsComponent } from './Views/card-skills/card-skills.component';
 import { PortifolioComponent } from './Views/portifolio/portifolio.component';
 
 @NgModule({
